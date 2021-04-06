7b0555a01bdd53c89cafe372359979b41f498e0a
Jonny221201 <abdulaziz221201@gmail.com>
2021-04-06 14:37:09 +0500

    ready

--- index.js
+++ index.js
@@ -11,5 +11,5 @@ app.use(logger);
 app.use(authentication);
 app.use("/api/products", products);
 
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`));
